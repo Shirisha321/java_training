a6db30ea6e9c3dd41ea039bed511876e49900106
sv680 <sv680@Shirisha>
2025-05-18 21:36:24 +0530

    First commit

---
+++ java_training2/src/com/atriaday3/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.atriaday3;
+
+public class FirstProgram {
+	System.out.println("my first program");
+	System.out.println("C2tc program");
+	System.out.print("java programming ");
+	System.out.println(" from tns ");
+	
+
+}
---
+++ java_training2/src/com/atriaday3/java_day3.java
@@ -0,0 +1,10 @@
+package com.atriaday3;
+
+public class java_day3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
